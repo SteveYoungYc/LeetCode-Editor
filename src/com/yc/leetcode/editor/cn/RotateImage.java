d26719b48ed29970629e670e69fa9f09c4c29a8b
Yc <STM32_Yc@sjtu.edu.cn>
2022-04-19 18:17:41 +0800

    optimize: RotateImage

--- src/com/yc/leetcode/editor/cn/RotateImage.java
+++ src/com/yc/leetcode/editor/cn/RotateImage.java
@@ -11,34 +11,16 @@ class RotateImage {
 
         public void rotate(int[][] matrix) {
             n = matrix.length;
-            if (n % 2 == 1) {
-                for (int i = 0; i <= n / 2; i++) {
-                    for (int j = 1; j <= n / 2; j++) {
-                        rotateOdd(matrix, i, j);
-                    }
-                }
-            } else {
-                for (int i = 0; i < n / 2; i++) {
-                    for (int j = 0; j < n / 2; j++) {
-                        rotateEven(matrix, i, j);
-                    }
+            int rowLen = n / 2;
+            int colLen = n / 2 + n % 2;
+            for (int i = 0; i < rowLen; i++) {
+                for (int j = 0; j < colLen; j++) {
+                    rotateFour(matrix, i, j);
                 }
             }
         }
 
-        private void rotateOdd(int[][] matrix, int i, int j) {
-            int offset = n / 2;
-            int a = matrix[offset + i][offset + j];
-            int b = matrix[offset + j][offset - i];
-            int c = matrix[offset - i][offset - j];
-            int d = matrix[offset - j][offset + i];
-            matrix[offset + i][offset + j] = d;
-            matrix[offset + j][offset - i] = a;
-            matrix[offset - i][offset - j] = b;
-            matrix[offset - j][offset + i] = c;
-        }
-
-        private void rotateEven(int[][] matrix, int i, int j) {
+        private void rotateFour(int[][] matrix, int i, int j) {
             int offset = n - 1;
             int a = matrix[i][j];
             int b = matrix[j][offset - i];
