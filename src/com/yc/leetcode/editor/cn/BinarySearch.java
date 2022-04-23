07a8e772718c53171262a0f79859f15cfbcd3041
Yc <STM32_Yc@sjtu.edu.cn>
2022-04-23 09:42:05 +0800

    BinarySearch

---
+++ src/com/yc/leetcode/editor/cn/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.yc.leetcode.editor.cn;
+
+class BinarySearch {
+    public static void main(String[] args) {
+        Solution solution = new BinarySearch().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0, right = nums.length - 1, mid;
+            while (left <= right) {
+                mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
