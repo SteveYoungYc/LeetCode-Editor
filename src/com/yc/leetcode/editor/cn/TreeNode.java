6182bc72d478cbf833b4123b41471a7c8987d932
Yc <STM32_Yc@sjtu.edu.cn>
2022-06-27 11:30:39 +0800

    add: print tree

--- src/com/yc/leetcode/editor/cn/TreeNode.java
+++ src/com/yc/leetcode/editor/cn/TreeNode.java
@@ -1,5 +1,8 @@
 package com.yc.leetcode.editor.cn;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -7,5 +10,31 @@ public class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    static void print(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode node;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                node = queue.poll();
+                if (node != null) {
+                    System.out.print(" " + node.val + " ");
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                } else {
+                    System.out.print(" n ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
