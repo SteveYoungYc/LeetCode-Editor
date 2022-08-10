819e3560c3a787f75f9d7b5a57ba63a59c09a31f
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-10 11:02:39 +0800

    EditDistance

--- src/com/yc/leetcode/editor/cn/EditDistance.java
+++ src/com/yc/leetcode/editor/cn/EditDistance.java
@@ -8,29 +8,28 @@ class EditDistance {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int minDistance(String s1, String s2) {
-            int m = s1.length(), n = s2.length();
-            // i，j 初始化指向最后一个索引
-            return dp(s1, m - 1, s2, n - 1);
-        }
 
-        // 定义：返回 s1[0..i] 和 s2[0..j] 的最小编辑距离
-        int dp(String s1, int i, String s2, int j) {
-            // base case
-            if (i == -1) return j + 1;  // s1走到头，剩余的操作就是将s2剩余的字母全部添加
-            if (j == -1) return i + 1;  // s2走到头，剩余的操作就是将s1剩余的字母全部删除
-
-            if (s1.charAt(i) == s2.charAt(j)) {
-                return dp(s1, i - 1, s2, j - 1); // 啥都不做
+        public int minDistance(String word1, String word2) {
+            int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+            dp[0][0] = 0;
+            for (int i = 1; i < word1.length() + 1; i++) {
+                dp[i][0] = i;
             }
-            return min(
-                    dp(s1, i, s2, j - 1) + 1,    // 插入
-                    dp(s1, i - 1, s2, j) + 1,    // 删除
-                    dp(s1, i - 1, s2, j - 1) + 1 // 替换
-            );
+            for (int i = 1; i < word2.length() + 1; i++) {
+                dp[0][i] = i;
+            }
+            for (int i = 1; i < word1.length() + 1; i++) {
+                for (int j = 1; j < word2.length() + 1; j++) {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1))
+                        dp[i][j] = dp[i - 1][j - 1];
+                    else
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+                }
+            }
+            return dp[word1.length()][word2.length()];
         }
 
-        int min(int a, int b, int c) {
+        private int min(int a, int b, int c) {
             return Math.min(a, Math.min(b, c));
         }
     }
