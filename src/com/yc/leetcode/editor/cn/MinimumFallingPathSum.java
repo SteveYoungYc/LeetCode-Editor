fbf5f98cb68957f03cc33ec67e6fb55571048440
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-10 10:10:53 +0800

    Optimize: MinimumFallingPathSum

--- src/com/yc/leetcode/editor/cn/MinimumFallingPathSum.java
+++ src/com/yc/leetcode/editor/cn/MinimumFallingPathSum.java
@@ -51,20 +51,18 @@ public class MinimumFallingPathSum {
             int m = matrix.length;
             int n = matrix[0].length;
             int[][] dp = new int[m][n];
-            int minSum = matrix[0][0];
+            int minSum = Integer.MAX_VALUE;
             System.arraycopy(matrix[0], 0, dp[0], 0, n);
             for (int i = 1; i < m; i++) {
-                dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][0];
-                if (i == m - 1)
-                    minSum = dp[i][0];
-                for (int j = 1; j < n - 1; j++) {
+                dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][0]; // 最后一列第一个位置
+                for (int j = 1; j < n - 1; j++) {   // 最后一列中间位置
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i - 1][j + 1]) + matrix[i][j];
-                    if (i == m - 1)
-                        minSum = Math.min(minSum, dp[i][j]);
                 }
-                dp[i][n - 1] = Math.min(dp[i - 1][n - 2], dp[i - 1][n - 1]) + matrix[i][n - 1];
-                if (i == m - 1)
-                    minSum = Math.min(minSum, dp[i][n - 1]);
+                dp[i][n - 1] = Math.min(dp[i - 1][n - 2], dp[i - 1][n - 1]) + matrix[i][n - 1]; // 最后一列最后一个位置
+            }
+            for (int i = 0; i < n; i++) {   // 取最后一列的最小值
+                if (dp[m - 1][i] < minSum)
+                    minSum = dp[m - 1][i];
             }
             return minSum;
         }
