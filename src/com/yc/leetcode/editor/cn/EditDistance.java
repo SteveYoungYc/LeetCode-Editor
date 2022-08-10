463453f3d2b8c99b7a08dc4256cd3356809dbfc8
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-10 11:29:58 +0800

    Comment: EditDistance

--- src/com/yc/leetcode/editor/cn/EditDistance.java
+++ src/com/yc/leetcode/editor/cn/EditDistance.java
@@ -23,7 +23,9 @@ class EditDistance {
                     if (word1.charAt(i - 1) == word2.charAt(j - 1))
                         dp[i][j] = dp[i - 1][j - 1];
                     else
-                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+                        dp[i][j] = min( dp[i - 1][j],   // 从dp[i - 1][j]到dp[i][j]说明跳过了word1的一个字符，即删除
+                                        dp[i][j - 1],   // 从dp[i][j - 1]到dp[i][j]说明跳过了word2的一个字符，即插入
+                                        dp[i - 1][j - 1]) + 1;
                 }
             }
             return dp[word1.length()][word2.length()];
