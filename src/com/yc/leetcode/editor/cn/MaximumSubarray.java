0bcb13cae84567e4a2dbb4ad356d90325c38a06f
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-11 10:27:22 +0800

    Optimize: MaximumSubarray

--- src/com/yc/leetcode/editor/cn/MaximumSubarray.java
+++ src/com/yc/leetcode/editor/cn/MaximumSubarray.java
@@ -8,17 +8,17 @@ class MaximumSubarray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxSubArray(int[] nums) {
-            int res;
-            int[] dp = new int[nums.length];    // dp[i]表示以nums[i]结尾的最大子数组和
-            dp[0] = nums[0];
-            res = dp[0];
+            int res, sum;
+            int lastSum = nums[0];
+            res = nums[0];
             for (int i = 1; i < nums.length; i++) {
-                if (dp[i - 1] < 0)
-                    dp[i] = nums[i];
+                if (lastSum < 0)
+                    sum = nums[i];
                 else
-                    dp[i] = dp[i - 1] + nums[i];
-                if (dp[i] > res)
-                    res = dp[i];
+                    sum = lastSum + nums[i];
+                lastSum = sum;
+                if (sum > res)
+                    res = sum;
             }
             return res;
         }
