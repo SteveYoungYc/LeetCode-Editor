8a47d045d05e47710e53e89c6239db4a68ae8822
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-15 19:22:42 +0800

    optimize: LongestPalindromicSubsequence

--- src/com/yc/leetcode/editor/cn/LongestPalindromicSubsequence.java
+++ src/com/yc/leetcode/editor/cn/LongestPalindromicSubsequence.java
@@ -8,18 +8,17 @@ class LongestPalindromicSubsequence {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestPalindromeSubseq(String s) {
-            String reverse = new StringBuffer(s).reverse().toString();
             int n = s.length() + 1;
             int[][] dp = new int[n][n];
             for (int i = 1; i < n; i++) {
-                for (int j = 1; j < n; j++) {
-                    if (s.charAt(i - 1) == reverse.charAt(j - 1))
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                for (int j = n - 2; j >= 0; j--) {
+                    if (s.charAt(i - 1) == s.charAt(j))
+                        dp[i][j] = dp[i - 1][j + 1] + 1;
                     else
-                        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                        dp[i][j] = Math.max(dp[i][j + 1], dp[i - 1][j]);
                 }
             }
-            return dp[n - 1][n - 1];
+            return dp[n - 1][0];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
