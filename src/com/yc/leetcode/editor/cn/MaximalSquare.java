c122c0dd3842211f193ba0716e7eb1d8e106b132
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-16 19:55:24 +0800

    MaximalSquare

--- src/com/yc/leetcode/editor/cn/MaximalSquare.java
+++ src/com/yc/leetcode/editor/cn/MaximalSquare.java
@@ -1,7 +1,5 @@
 package com.yc.leetcode.editor.cn;
 
-import java.util.Arrays;
-
 public class MaximalSquare {
     public static void main(String[] args) {
         Solution solution = new MaximalSquare().new Solution();
@@ -13,44 +11,16 @@ public class MaximalSquare {
             int m = matrix.length;
             int n = matrix[0].length;
             int maxSquare = matrix[0][0] - '0';
-            int[][] dp = new int[m][n];
-            dp[0][0] = matrix[0][0] - '0';
-            for (int i = 1; i < m; i++) {
-                dp[i][0] = matrix[i][0] - '0';
-                if (matrix[i][0] == '1')
-                    maxSquare = 1;
-            }
-            for (int i = 1; i < n; i++) {
-                dp[0][i] = matrix[0][i] - '0';
-                if (matrix[0][i] == '1')
-                    maxSquare = 1;
-            }
-            for (int i = 1; i < m; i++) {
-                for (int j = 1; j < n; j++) {
-                    if (matrix[i][j] == '1') {
-                        dp[i][j] = 1;
-                        maxSquare = Math.max(maxSquare, dp[i][j]);
-                        if (dp[i - 1][j - 1] >= 1 && isOne(matrix, i, j, dp[i - 1][j - 1])) {
-                            dp[i][j] = dp[i - 1][j - 1] + 1;
-                            maxSquare = Math.max(maxSquare, dp[i][j]);
-                        }
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 1; i < m + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (matrix[i - 1][j - 1] == '1') {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                     }
+                    maxSquare = Math.max(maxSquare, dp[i][j]);
                 }
             }
-            System.out.println(Arrays.deepToString(dp));
             return maxSquare * maxSquare;
-        }
-
-        private boolean isOne(char[][] matrix, int i, int j, int length) {
-            for (int k = i - length; k < i; k++) {
-                if (matrix[k][j] == '0')
-                    return false;
-            }
-            for (int k = j - length; k < j; k++) {
-                if (matrix[i][k] == '0')
-                    return false;
-            }
-            return true;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
