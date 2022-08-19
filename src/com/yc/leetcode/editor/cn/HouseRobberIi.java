9af73394329f2369b5e9f9d89556caf7f99b68e6
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-19 09:21:13 +0800

    optimize: HouseRobberII

--- src/com/yc/leetcode/editor/cn/HouseRobberIi.java
+++ src/com/yc/leetcode/editor/cn/HouseRobberIi.java
@@ -26,16 +26,11 @@ class HouseRobberIi {
             // 抢尾不枪头，0不抢，len-1抢，len-2不抢
             dp[1] = nums[1];
             dp[2] = Math.max(nums[1], nums[2]);
-            for (int i = 3; i < len - 2; i++) {
+            for (int i = 3; i < len - 1; i++) {
                 dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
             }
             res = Math.max(res, dp[len - 3] + nums[len - 1]);
             // 头尾都不抢，0不抢，len-1不抢，len-2抢，1抢
-            dp[1] = nums[1];
-            dp[2] = Math.max(nums[1], nums[2]);
-            for (int i = 3; i < len - 1; i++) {
-                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
-            }
             res = Math.max(res, dp[len - 2]);
             return res;
         }
