5c85a388b8e5ad3f2cb6936d1a0d990c5d38e68e
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-22 19:22:14 +0800

    optimize: MergeKSortedLists

--- src/com/yc/leetcode/editor/cn/MergeKSortedLists.java
+++ src/com/yc/leetcode/editor/cn/MergeKSortedLists.java
@@ -1,25 +1,32 @@
 package com.yc.leetcode.editor.cn;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 class MergeKSortedLists {
     public static void main(String[] args) {
         Solution solution = new MergeKSortedLists().new Solution();
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public ListNode mergeKLists(ListNode[] lists) {
-            ListNode tail = new ListNode(0), head = tail;
-            Queue<Integer> queue = new PriorityQueue<>();
+            if (lists.length == 0)
+                return null;
+            ListNode head = new ListNode(-1), tail = head;
+            PriorityQueue<ListNode> queue = new PriorityQueue<>(    // 队列长度只有k
+                    lists.length, Comparator.comparingInt(a -> a.val));
             for (ListNode node : lists) {
-                while (node != null) {
-                    queue.offer(node.val);
-                    node = node.next;
+                if (node != null) {
+                    queue.offer(node);
                 }
             }
             while (!queue.isEmpty()) {
-                tail.next = new ListNode(queue.poll());
+                ListNode node = queue.poll();
+                tail.next = node;
+                if (node.next != null) {
+                    queue.offer(node.next);
+                }
                 tail = tail.next;
             }
             return head.next;
