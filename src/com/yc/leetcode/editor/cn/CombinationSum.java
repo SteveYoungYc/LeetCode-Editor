758a981bc009b8cf6694087836dcf6bad208f59b
Yc <STM32_Yc@sjtu.edu.cn>
2022-08-25 19:25:41 +0800

    CombinationSum

--- src/com/yc/leetcode/editor/cn/CombinationSum.java
+++ src/com/yc/leetcode/editor/cn/CombinationSum.java
@@ -9,36 +9,29 @@ class CombinationSum {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        List<List<Integer>> lists = new ArrayList<>();
-        HashMap<Integer, List<Integer>> dp = new HashMap<>();
+        List<List<Integer>> lists = new LinkedList<>();
+        LinkedList<Integer> track = new LinkedList<>();
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            for (int i = 0; i <= target; i++) {
-                List<Integer> list = new ArrayList<>();
-                dp.put(i, list);
-                for (int candidate : candidates) {
-                    int last = i - candidate;
-                    if ((last > 0 && !dp.get(last).isEmpty()) || last == 0) {
-                        list.add(candidate);
-                    }
-                }
-            }
-            boolean[] used = new boolean[target + 1];
-            backtrack(new LinkedList<>(), target, used);
+            Arrays.sort(candidates);
+            backtrace(candidates, 0, target);
             return lists;
         }
 
-        private void backtrack(LinkedList<Integer> track, int target, boolean[] used) {
-            System.out.println(target + " " + track);
-            if (target == 0)
-                lists.add(new LinkedList(track));
-            List<Integer> list = dp.get(target);
-            for (Integer i : list) {
-                if (used[i]) {
+        private void backtrace(int[] candidates, int start, int target) {
+            if (target == 0) {
+                lists.add(new LinkedList<>(track));
+                return;
+            }
+            if (target < 0)
+                return;
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i - 1])
                     continue;
-                }
-                track.add(i);
-                backtrack(track, target - i, used);
+                track.add(candidates[i]);
+                target -= candidates[i];
+                backtrace(candidates, i, target);
                 track.removeLast();
+                target += candidates[i];
             }
         }
     }
