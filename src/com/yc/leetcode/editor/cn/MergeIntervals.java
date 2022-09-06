e2024613317922b4133e5a67a17fee297c9c38cb
Yc <STM32_Yc@sjtu.edu.cn>
2022-09-06 17:30:12 +0800

    MergeIntervals

---
+++ src/com/yc/leetcode/editor/cn/MergeIntervals.java
@@ -0,0 +1,69 @@
+package com.yc.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+class MergeIntervals {
+    public static void main(String[] args) {
+        Solution solution = new MergeIntervals().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        private class Boundary implements Comparable<Boundary> {
+            int val;
+            int type;
+
+            public Boundary(int val, int type) {
+                this.val = val;
+                this.type = type;
+            }
+
+            @Override
+            public int compareTo(Boundary o) {
+                if (this.val == o.val) {
+                    return Integer.compare(this.type, o.type);
+                }
+                return Integer.compare(this.val, o.val);
+            }
+        }
+
+        public int[][] merge(int[][] intervals) {
+            int[][] res;
+            PriorityQueue<Boundary> queue = new PriorityQueue<>();
+            Stack<Boundary> stack = new Stack<>();
+            List<int[]> lists = new ArrayList<>();
+            for (int[] interval : intervals) {
+                queue.offer(new Boundary(interval[0], 0));
+                queue.offer(new Boundary(interval[1], 1));
+            }
+            assert queue.peek() != null;
+            int minVal = queue.peek().val;
+            while (!queue.isEmpty()) {
+                Boundary boundary = queue.poll();
+                if (minVal == -1)
+                    minVal = boundary.val;
+                if (boundary.type == 0) {
+                    stack.add(boundary);
+                }
+                if (boundary.type == 1) {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        lists.add(new int[]{minVal, boundary.val});
+                        minVal = -1;
+                    }
+                }
+            }
+            res = new int[lists.size()][2];
+            for (int i = 0; i < lists.size(); i++) {
+                res[i][0] = lists.get(i)[0];
+                res[i][1] = lists.get(i)[1];
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
